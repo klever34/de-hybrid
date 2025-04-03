b0ea2920531ea629d1631f3b3cbfc84e0fbc73ae
Lanre Adedara <lanre.adedara@shara.co>
2025-04-03 06:51:27 +0100

    updated price

--- app/utils/book_list.ts
+++ app/utils/book_list.ts
@@ -5,7 +5,7 @@ export const staticBooks: Book[] = [
     id: "0",
     title: "Thank you My Mama",
     artist: "Roy Odoko",
-    price: 1800.0,
+    price: 2000.0,
     imageUrl: "/music-icon-bg.jpg",
     fileUrl:
       "https://res.cloudinary.com/dxibkvmpl/video/upload/v1733752470/thank_you_my_mama_vxn9yo.mp3",
@@ -14,7 +14,7 @@ export const staticBooks: Book[] = [
     id: "1",
     title: "What a Great Day",
     artist: "Roy Odoko",
-    price: 1800.0,
+    price: 2000.0,
     imageUrl: "/music-icon-bg.jpg",
     fileUrl:
       "https://res.cloudinary.com/dxibkvmpl/video/upload/v1733753198/what_a_great_day_a3l8ku.mp3",
@@ -23,7 +23,7 @@ export const staticBooks: Book[] = [
     id: "2",
     title: "Koko Na Nigger",
     artist: "Roy Odoko",
-    price: 1800.0,
+    price: 2000.0,
     imageUrl: "/music-icon-bg.jpg",
     fileUrl:
       "https://res.cloudinary.com/dxibkvmpl/video/upload/v1733755889/koko_na_nigger_zd2ugu.mp3",
@@ -32,7 +32,7 @@ export const staticBooks: Book[] = [
     id: "3",
     title: "Staircase Singer",
     artist: "Roy Odoko",
-    price: 1800.0,
+    price: 2000.0,
     imageUrl: "/music-icon-bg.jpg",
     fileUrl:
       "https://res.cloudinary.com/dxibkvmpl/video/upload/v1734605952/staircase_singer_rxthhb.mp3",
@@ -41,7 +41,7 @@ export const staticBooks: Book[] = [
     id: "4",
     title: "Official Corruption",
     artist: "Roy Odoko",
-    price: 1800.0,
+    price: 2000.0,
     imageUrl: "/music-icon-bg.jpg",
     fileUrl:
       "https://res.cloudinary.com/dxibkvmpl/video/upload/v1734608714/official_corruption_ooytuk.mp3",
@@ -50,7 +50,7 @@ export const staticBooks: Book[] = [
     id: "5",
     title: "Mr Fumbler",
     artist: "Roy Odoko",
-    price: 1800.0,
+    price: 2000.0,
     imageUrl: "/music-icon-bg.jpg",
     fileUrl:
       "https://res.cloudinary.com/dxibkvmpl/video/upload/v1734608713/mr_fumbler_u3rqsv.mp3",
@@ -59,7 +59,7 @@ export const staticBooks: Book[] = [
     id: "6",
     title: "Mr Lazy Man",
     artist: "Roy Odoko",
-    price: 1800.0,
+    price: 2000.0,
     imageUrl: "/music-icon-bg.jpg",
     fileUrl:
       "https://res.cloudinary.com/dxibkvmpl/video/upload/v1734608714/mr_lazy_man_ykfido.mp3",
@@ -68,7 +68,7 @@ export const staticBooks: Book[] = [
     id: "7",
     title: "Kokobiliko",
     artist: "Roy Odoko",
-    price: 1800.0,
+    price: 2000.0,
     imageUrl: "/music-icon-bg.jpg",
     fileUrl:
       "https://res.cloudinary.com/dxibkvmpl/video/upload/v1734608714/mr_lazy_man_ykfido.mp3",
@@ -77,7 +77,7 @@ export const staticBooks: Book[] = [
     id: "8",
     title: "Jesus Christ is Lord",
     artist: "Roy Odoko",
-    price: 1800.0,
+    price: 2000.0,
     imageUrl: "/music-icon-bg.jpg",
     fileUrl:
       "https://res.cloudinary.com/dxibkvmpl/video/upload/v1734609076/jesus_christ_is_lord_edhx1i.mp3",
@@ -86,7 +86,7 @@ export const staticBooks: Book[] = [
     id: "9",
     title: "I Get One Friend",
     artist: "Roy Odoko",
-    price: 1800.0,
+    price: 2000.0,
     imageUrl: "/music-icon-bg.jpg",
     fileUrl:
       "https://res.cloudinary.com/dxibkvmpl/video/upload/v1734609261/i_get_one_friend_inxn8g.mp3",
--- app/utils/videos_list.ts
+++ app/utils/videos_list.ts
@@ -5,7 +5,7 @@ export const staticVideos: Book[] = [
     id: "0",
     title: "Fatty Bum Bum",
     artist: "Roy Odoko",
-    price: 1800.0,
+    price: 2000.0,
     imageUrl: "/music-icon-bg.jpg",
     fileUrl:
       "https://res.cloudinary.com/dxibkvmpl/video/upload/v1743248745/fatty_bum_bum_ycjeb7.mov",
